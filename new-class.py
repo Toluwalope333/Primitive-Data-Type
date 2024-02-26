ef7b9f888b130b533abbd0b4ffb4273d9487d0f0
Olaleye Tolu <toluwalopeolaleye@gmail.com>
2024-02-26 12:33:48 +0100

    My First Primitive Data Type Lecture in Python Aptech

---
+++ new-class.py
@@ -0,0 +1,23 @@
+print("sis tolu")
+print('sis tolu')
+name="sister tolu"
+print(name)
+
+title="sis "
+name="tolu"
+both = title + name 
+#or both=title + "" + name
+print(both.upper())
+
+#for capitalised it changes only the 1st letter
+print(both.capitalize())
+
+print("Welcome to Mama charle store")
+print("Input what you would like to buy below")
+product_amount=1000
+cost_of_product=650
+change= product_amount - cost_of_product
+
+buyer = "sis tolu"
+
+print(buyer.title() + " received", change, "change")
